d62a9ded826169e11586bf7268d849767b784d1f
Sumit Raurale <25202100+sumitraurale@users.noreply.github.com>
2020-01-22 12:43:15 +0000

    Update mlp_classify.m

--- mlp_classify.m
+++ mlp_classify.m
@@ -5,17 +5,16 @@
 % Syntax: [class_out] = mlp_classify(feat_proj)
 %
 % Inputs: 
-%     feat_proj    - projected feature vector from LDA (size 8 x 1)
+%     feat_proj  - projected feature vector from LDA (size 8 x 1)
 %
 % Outputs: 
-%     class_out    - evaluated feature class vector (size 9 x 1)
+%     class_out  - evaluated feature class vector (size 9 x 1)
 %
 %
 % Author: Sumit Raurale, Ph.D.
 %         email: sumit.raurale@gmail.com
 %         ECIT, Queen's University Belfast, United Kingdom
 %-----------------------------------------------------------------------------------
-
 
 function [class_out] = mlp_classify(feat_proj)  %#codegen
 
@@ -26,23 +25,23 @@ Q = 1; % samples/series
 
 % Allocate Outputs
 
-    % Input 1
-    Xp1 = mapminmax_apply(feat_proj,x1_step1);
+% Input 1
+Xp1 = mapminmax_apply(feat_proj,x1_step1);
     
-    % Layer 1
-    a1 = tansig_apply(repmat(b1,1,Q) + IW1_1*Xp1);
+% Layer 1
+a1 = tansig_apply(repmat(b1,1,Q) + IW1_1*Xp1);
     
-    % Layer 2
-    a2 = tansig_apply(repmat(b2,1,Q) + LW2_1*a1);
+% Layer 2
+a2 = tansig_apply(repmat(b2,1,Q) + LW2_1*a1);
     
-    % Layer 3
-    a3 = tansig_apply(repmat(b3,1,Q) + LW3_2*a2);
+% Layer 3
+a3 = tansig_apply(repmat(b3,1,Q) + LW3_2*a2);
     
-    % Layer 4
-    a4 = repmat(b4,1,Q) + LW4_3*a3;
+% Layer 4
+a4 = repmat(b4,1,Q) + LW4_3*a3;
     
-    % Output 1
-    class_out = mapminmax_reverse(a4,y1_step1);
+% Output 1
+class_out = mapminmax_reverse(a4,y1_step1);
 
 
 % Format Output Arguments
